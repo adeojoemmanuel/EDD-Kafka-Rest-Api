823599c0570168af1a0f35ce67389f4414296205
adeojoemmanueli <emmanuel@xroads.tech>
2024-08-06 20:06:08 +0100

    feat: separating repo to work as a standalone application, by creating a mono-package.json

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const DataBaseConnection = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/auth-system', {});
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/edd-system-db', {});
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
